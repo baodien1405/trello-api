7e2447871a134c078a3dccc3c83c749044999d46
Dien Cap <baodien1405@gmail.com>
2024-11-21 22:37:36 +0700

    fix: Update method API logout

--- src/routes/v1/auth.route.ts
+++ src/routes/v1/auth.route.ts
@@ -10,7 +10,7 @@ const router = express.Router()
 router.post('/users/register', validator(authSchema.register), asyncHandler(AuthController.register))
 router.post('/users/login', validator(authSchema.login), asyncHandler(AuthController.login))
 router.put('/users/verify', validator(authSchema.verify), asyncHandler(AuthController.verify))
-router.post('/users/logout', asyncHandler(AuthController.logout))
+router.delete('/users/logout', asyncHandler(AuthController.logout))
 router.get('/users/refreshToken', asyncHandler(AuthController.refreshToken))
 
 export { router as AuthRoute }
