020ca5089372062b2a60b5cefb9a04d2df6a55e4
avan <avan.cap@vortechinc.io>
2026-08-08 08:07:48 +0700

    fix: change config eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,16 +1,24 @@
+
+import { defineConfig, globalIgnores } from 'eslint/config'
 import tseslint from 'typescript-eslint'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
-export default tseslint.config(
-  {
-    ignores: ['node_modules/**', 'dist/**']
-  },
-  ...tseslint.configs.recommended,
-  eslintPluginPrettierRecommended,
+export default defineConfig(
+  globalIgnores(['node_modules/**', 'dist/**']),
+
+  tseslint.configs.recommended,
+
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off'
+    }
+  },
+
+  eslintPluginPrettierRecommended,
+
+  {
+    rules: {
       'prettier/prettier': [
         'warn',
         {
@@ -28,3 +36,4 @@ export default tseslint.config(
     }
   }
 )
+
