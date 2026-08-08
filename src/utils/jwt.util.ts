6f2d62a6541c7d4f8d3fac6b5560f1bc838740f6
avan <avan.cap@vortechinc.io>
2026-08-08 20:37:57 +0700

    chore: update JWT expiration durations

--- src/utils/jwt.util.ts
+++ src/utils/jwt.util.ts
@@ -3,8 +3,8 @@ import { AuthPayload } from '@/types'
 import { env } from '@/config'
 
 export const createTokenPair = (payload: AuthPayload) => {
-  const accessToken = jwt.sign(payload, env.ACCESS_TOKEN_SECRET_SIGNATURE as string, { expiresIn: '1h' })
-  const refreshToken = jwt.sign(payload, env.REFRESH_TOKEN_SECRET_SIGNATURE as string, { expiresIn: '14 days' })
+  const accessToken = jwt.sign(payload, env.ACCESS_TOKEN_SECRET_SIGNATURE as string, { expiresIn: '1d' })
+  const refreshToken = jwt.sign(payload, env.REFRESH_TOKEN_SECRET_SIGNATURE as string, { expiresIn: '7d' })
 
   return { accessToken, refreshToken }
 }
